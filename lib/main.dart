680adbc96936fe142c8011a01e4ec902091bc101
Jordan Jung <yungeol2016@gmail.com>
2024-12-16 19:43:23 +0800

    chatlist, chatroom, mypage 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:graduation_project_1/screen/home_screen.dart';
 import 'package:graduation_project_1/screen/login_screen.dart';
+import 'package:graduation_project_1/screen/chatlist_screen.dart'
+import 'package:graduation_project_1/screen/chatroom_screen.dart'
+import 'package:graduation_project_1/screen/mypage_screen.dart';
 import 'package:graduation_project_1/screen/signup_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
